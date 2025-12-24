e9e0c21b4924abc0d72629cd2f39fa1d1e15b946
Akshay_Dev <akshay.nullvoid@gmail.com>
2025-12-24 14:36:40 +0530

    Update main.cpp and added few lines for convenience of the code

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 using ll  = long long;
 using ull = unsigned long long;
 using ld  = long double;
-
+//Pairs
 using pii = pair<int,int>;
 using pll = pair<ll,ll>;
 using psi = pair<string,int>;
 using pis = pair<int,string>;
-
+//Vectors
 using vi  = vector<int>;
 using vll = vector<ll>;
 using vb  = vector<bool>;
@@ -18,12 +18,25 @@ using vs  = vector<string>;
 using vpii = vector<pii>;
 using vpll = vector<pll>;
 
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef vector<pii> vpii;
+typedef vector<pll> vpll;
+
 #define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 #define sz(x) (int)(x).size()
 #define ff first
 #define ss second
+#define ppb pop_back
+#define endl '\n'
+#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
+#define ROF(i,a,b) for(int i = (a); i >= (b); --i)
 
 const int INF = 1e9;
 const ll LINF = 1e18;
@@ -34,6 +47,16 @@ const int MOD = 1e9 + 7;
 #else
     #define debug(x)
 #endif
+
+// Helper functions for convenience
+ll mod_mul(ll a, ll b, ll m = MOD){ return (a*b)%m; }
+ll mod_add(ll a, ll b, ll m = MOD){ return (a+b)%m; }
+ll mod_sub(ll a, ll b, ll m = MOD){ return ((a-b)%m + m) % m; }
+void _print(int x){cerr << x;}
+void _print(ll x){cerr << x;}
+void _print(ld x){cerr << x;}
+void _print(string x){cerr << x;}
+void _print(char x){cerr << x;}
 
 ll gcd(ll a, ll b){
     while(b){
@@ -94,5 +117,6 @@ int main(){
     }
     return 0;
 }
+
 
 
