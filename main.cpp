5e3f6a64b427fc68944bfb2cd0bb0f323ce84683
Akshay_Dev <akshay.nullvoid@gmail.com>
2025-12-24 09:33:06 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,62 +1,98 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-typedef long double ld;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vll;
-typedef vector<pii> vpii;
-typedef vector<pll> vpll;
+using ll  = long long;
+using ull = unsigned long long;
+using ld  = long double;
 
-#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr)
+using pii = pair<int,int>;
+using pll = pair<ll,ll>;
+using psi = pair<string,int>;
+using pis = pair<int,string>;
+
+using vi  = vector<int>;
+using vll = vector<ll>;
+using vb  = vector<bool>;
+using vs  = vector<string>;
+
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+#define fastio() ios::sync_with_stdio(false); cin.tie(nullptr)
 #define pb push_back
-#define mp make_pair
 #define all(x) (x).begin(), (x).end()
-#define sz(x) ((int)(x).size())
+#define sz(x) (int)(x).size()
 #define ff first
 #define ss second
-#define endl '\n'
-#define FOR(i,a,b) for(int i = (a); i < (b); ++i)
-#define ROF(i,a,b) for(int i = (a); i >= (b); --i)
 
-const int INF = 1e9+7;
+const int INF = 1e9;
 const ll LINF = 1e18;
-const int MOD = 1e9+7;
-const ld PI = acos(-1.0);
-
+const int MOD = 1e9 + 7;
 
 #ifdef LOCAL
-    #define debug(x) cerr << #x <<" = "; _print(x); cerr << endl;
+    #define debug(x) cerr << #x << " = " << x << '\n'
 #else
     #define debug(x)
 #endif
 
-void _print(int x){cerr << x;}
-void _print(ll x){cerr << x;}
-void _print(ld x){cerr << x;}
-void _print(string x){cerr << x;}
-void _print(char x){cerr << x;}
-template<typename T, typename V> void _print(pair<T,V> p){cerr<<"{";_print(p.ff);cerr<<",";_print(p.ss);cerr<<"}";}
-template<typename T> void _print(vector<T> v){cerr<<"[";for(auto i:v){_print(i);cerr<<" ";}cerr<<"]";}
-template<typename T> void _print(set<T> s){cerr<<"[";for(auto i:s){_print(i);cerr<<" ";}cerr<<"]";}
-template<typename T, typename V> void _print(map<T,V> m){cerr<<"[";for(auto i:m){_print(i);cerr<<" ";}cerr<<"]";}
+ll gcd(ll a, ll b){
+    while(b){
+        a %= b;
+        swap(a, b);
+    }
+    return a;
+}
 
-ll mod_mul(ll a, ll b, ll m = MOD){ return (a*b)%m; }
-ll mod_add(ll a, ll b, ll m = MOD){ return (a+b)%m; }
-ll mod_sub(ll a, ll b, ll m = MOD){ return ((a-b)%m + m) % m; }
+ll lcm(ll a, ll b){
+    return (a / gcd(a, b)) * b;
+}
+
+ll mod_pow(ll a, ll b){
+    ll res = 1;
+    a %= MOD;
+    while(b){
+        if(b & 1) res = (res * a) % MOD;
+        a = (a * a) % MOD;
+        b >>= 1;
+    }
+    return res;
+}
+
+ll mod_inv(ll a){
+    return mod_pow(a, MOD - 2);
+}
+
+bool chmax(ll &a, ll b){
+    if(b > a){ a = b; return true; }
+    return false;
+}
+
+bool chmin(ll &a, ll b){
+    if(b < a){ a = b; return true; }
+    return false;
+}
+
+void read_vec(vi &v, int n){
+    v.resize(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+}
+
+void read_vec(vll &v, int n){
+    v.resize(n);
+    for(int i = 0; i < n; i++) cin >> v[i];
+}
 
 void solve(){
-   //ans here
 }
 
 int main(){
     fastio();
     int t = 1;
-    // cin >> t; // uncomment if multiple test cases
+    // cin >> t;
     while(t--){
-    solve();
+        solve();
     }
     return 0;
 }
+
+
